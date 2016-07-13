2d2885e6dd6d7943308d923dfb66d07251764974
Shradha Pulla & Ryan Streur <pullashradha@gmail.com>
2016-07-13 10:40:08 -0700

    add a test for a Save() method for the Cuisine class, the Save() method for the cuisine class, and the DeleteAll() method for the cuisine class

--- Objects/Cuisine.cs
+++ Objects/Cuisine.cs
@@ -65,5 +65,36 @@ namespace RestaurantList
       }
       return allCuisines;
     }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("INSERT INTO cuisines (name) OUTPUT INSERTED.id VALUES (@CuisineName);", conn);
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@CuisineName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM cuisines;", conn);
+      cmd.ExecuteNonQuery();
+    }
   }
 }
--- Tests/CuisineTest.cs
+++ Tests/CuisineTest.cs
@@ -6,25 +6,37 @@ using Xunit;
 
 namespace RestaurantList
 {
-  public class CuisineTest
+  public class CuisineTest : IDisposable
   {
     public CuisineTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=restaurant_list_test;Integrated Security=SSPI;";
     }
     [Fact]
-    public void Test_EmptyDatabase_0()
+    public void CuisineTest_EmptyDatabase_0()
     {
       int numberOfCuisines = Cuisine.GetAll().Count;
       Assert.Equal(0, numberOfCuisines);
     }
-
     [Fact]
-    public void Cuisine_EqualIfNamesMatch_true()
+    public void CuisineTest_EqualIfNamesMatch_true()
     {
       Cuisine newCuisine1 = new Cuisine("Italian");
       Cuisine newCuisine2 = new Cuisine("Italian");
       Assert.Equal(newCuisine1, newCuisine2);
+    }
+    [Fact]
+    public void CuisineTest_Save_SaveAllCuisinesToDatabase()
+    {
+      Cuisine newCuisine = new Cuisine("Italian");
+      newCuisine.Save();
+      List<Cuisine> newCuisineList = new List<Cuisine> {newCuisine};
+      List<Cuisine> resultList = Cuisine.GetAll();
+      Assert.Equal(newCuisine, resultList[0]);
+    }
+    public void Dispose()
+    {
+      Cuisine.DeleteAll();
     }
   }
 }
