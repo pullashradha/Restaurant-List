3b07bbd6c8255ab2a4c6c64df5f96744976d17cb
Shradha Pulla & Ryan Streur <pullashradha@gmail.com>
2016-07-14 09:44:41 -0700

    Create restaurant_form.cshtml and restaurant_created.cshtml pages, link cuisine category names in cuisine list page to a restaurant list, error in styling for cuisine_list.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,16 +6,29 @@ namespace RestaurantList
   {
     public HomeModule()
     {
-      Get ["/"] =_=> View ["index.cshtml"];
-      Get["/cuisinelist"] =_=> View["cuisinelist.cshtml", Cuisine.GetAll()];
-      Get["/clearcuisines"] =_=> {
+      Get ["/"] = _ => View ["index.cshtml"];
+      Get ["/cuisine/list"] = _ => View ["cuisine_list.cshtml", Cuisine.GetAll()];
+      Get ["/cuisine/list/cleared"] = _ => {
         Cuisine.DeleteAll();
-        return View ["/cuisinelist", Cuisine.GetAll()];
+        return View ["cuisine_list.cshtml", Cuisine.GetAll()];
       };
       Post ["/cuisine/created"] = _ => {
         Cuisine newCuisine = new Cuisine (Request.Form["cuisine-name"]);
         newCuisine.Save();
         return View ["cuisine_created.cshtml"];
+      };
+      Get ["/restaurant/new"] = _ => View ["restaurant_form.cshtml", Cuisine.GetAll()];
+      Post ["/restaurant/created"] = _ => {
+        Restaurant newRestaurant = new Restaurant
+        (
+          Request.Form ["restaurant-name"],
+          Request.Form ["restaurant-address"],
+          Request.Form ["restaurant-phone-number"],
+          Request.Form ["restaurant-description"],
+          Request.Form ["cuisine-id"]
+        );
+        newRestaurant.Save();
+        return View ["restaurant_created.cshtml"];
       };
     }
   }
